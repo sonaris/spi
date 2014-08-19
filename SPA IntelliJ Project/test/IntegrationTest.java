9c300cbc48c1ce8ee4cd9f56d0ee37e7df2295c0
sonaris <david.overfeld@gmx.de>
2014-08-19 13:45:41 -0400

    test

---
+++ SPA IntelliJ Project/test/IntegrationTest.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+
+import play.test.*;
+import play.libs.F.*;
+
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+
+public class IntegrationTest {
+
+
+}
